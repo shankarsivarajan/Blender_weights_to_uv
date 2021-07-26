b6a5ec73a2b9992b8cad053c69a22f9471422ec4
Shankar Sivarajan <shankarnsivarajan@gmail.com>
2021-07-26 09:27:01 -0400

    Added option to convert all vertex groups.
    Blender limits  to number of allowed UV maps to eight.

--- weights_to_uv.py
+++ weights_to_uv.py
@@ -35,16 +35,31 @@ def bmesh_get_weight_numpy(group_index, layer, verts):
     return weight
 
 
-class Vertex_Weights_to_UV(bpy.types.Operator):
-    bl_idname = 'object.vert_weights_to_uv'
-    bl_label = 'Vertex Weights to UV'
+class Vertex_Weights_to_UV_Active(bpy.types.Operator):
+    """Convert active vertex group to UV Map"""
+    bl_idname = 'object.vert_weights_to_uv_active'
+    bl_label = 'Active Vertex Group'
     bl_options = {'REGISTER', 'UNDO'}
-    
+
+
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
+
     def execute(self, context):
         ob = context.active_object
         me = ob.data
         
+        loops_size = get_attribute_numpy(me.polygons, attribute='loop_total', mult=1)
+        n_data = np.sum(loops_size)
+        v_id = np.ones(n_data)
+        me.polygons.foreach_get('vertices',v_id)
+        v_id = v_id.astype(int)
+        
+        
         active_group = bpy.context.object.vertex_groups.active.name
+        print(active_group)
         
         weight = []
         for v in me.vertices:
@@ -62,12 +77,6 @@ class Vertex_Weights_to_UV(bpy.types.Operator):
         else:
             uv_layer = me.uv_layers.new(name=uv_layer_name)
             
-        loops_size = get_attribute_numpy(me.polygons, attribute='loop_total', mult=1)
-        n_data = np.sum(loops_size)
-        v_id = np.ones(n_data)
-        me.polygons.foreach_get('vertices',v_id)
-        v_id = v_id.astype(int)
-        
         uv_x = weight[v_id,None]
         uv_y = weight[v_id,None]
         
@@ -78,13 +87,66 @@ class Vertex_Weights_to_UV(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class Vertex_Weights_to_UV_All(bpy.types.Operator):
+    """Convert all vertex groups to UV Map (currently limited to 8 maps)"""
+    bl_idname = 'object.vert_weights_to_uv_all'
+    bl_label = 'All Vertex Groups'
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    @classmethod
+    def poll(cls, context):
+        return context.selected_objects
+
+
+    def execute(self, context):
+        ob = context.active_object
+        me = ob.data
+        
+        loops_size = get_attribute_numpy(me.polygons, attribute='loop_total', mult=1)
+        n_data = np.sum(loops_size)
+        v_id = np.ones(n_data)
+        me.polygons.foreach_get('vertices',v_id)
+        v_id = v_id.astype(int)
+        
+        
+        for vertex_group in bpy.context.object.vertex_groups:
+            current_group = vertex_group.name
+            print(current_group)
+            
+            weight = []
+            for v in me.vertices:
+                try:
+                    weight.append(ob.vertex_groups[current_group].weight(v.index))
+                except:
+                    weight.append(0)
+            
+            weight = np.array(weight)        
+            
+            uv_layer_name = current_group
+            
+            if uv_layer_name in me.uv_layers.keys():
+                uv_layer = me.uv_layers[uv_layer_name]
+            else:
+                uv_layer = me.uv_layers.new(name=uv_layer_name)
+                
+            uv_x = weight[v_id,None]
+            uv_y = weight[v_id,None]
+            
+            uv = np.concatenate((uv_x,uv_y),axis=1).flatten()
+            
+            uv_layer.data.foreach_set('uv',uv)
+            me.uv_layers.update()
+
+        return {'FINISHED'}
+
 
 class WeightsToUV_Menu(bpy.types.Menu):
     bl_label = 'Vertex Weights to UV'
     bl_idname = 'OBJECT_MT_vert_weights_to_uv'
 
     def draw(self, context):
-        self.layout.operator(Vertex_Weights_to_UV.bl_idname)
+        self.layout.operator(Vertex_Weights_to_UV_Active.bl_idname)
+        self.layout.operator(Vertex_Weights_to_UV_All.bl_idname)
 
 
 def menu_func(self, context):
@@ -93,16 +155,20 @@ def menu_func(self, context):
 
 def register():
     bpy.utils.register_class(WeightsToUV_Menu)
-    bpy.utils.register_class(Vertex_Weights_to_UV)
+    
+    bpy.utils.register_class(Vertex_Weights_to_UV_Active)
+    bpy.utils.register_class(Vertex_Weights_to_UV_All)
+    
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 
 def unregister():
     bpy.utils.unregister_class(WeightsToUV_Menu)
 
-    bpy.utils.unregister_class(Vertex_Weights_to_UV)
-    bpy.types.VIEW3D_MT_object.remove(menu_func)
+    bpy.utils.unregister_class(Vertex_Weights_to_UV_Active)
+    bpy.utils.unregister_class(Vertex_Weights_to_UV_All)
 
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
 
 if __name__ == "__main__":
   register()
